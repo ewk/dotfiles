42a50f9f2b0ec7e4441c1c4f8eeb42f9bbb732b6
Eddie Kovsky <edkovsky@gmail.com>
2013-03-24 14:43:22 -0600

    Added configuration for emacs auto-complete.
    Installation is a pain!! ELPA does not include this package yet. Let yum handle
    installation for me. Not sure what to do on OS X ...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -100,3 +100,8 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
+
+;; Auto-complete
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
