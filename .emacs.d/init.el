a9bf4b1509ce88f1d290ab4812eee690ea04a347
Eddie Kovsky <ewk@edkovsky.org>
2014-09-11 19:42:37 -0600

    Emacs Magit package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -116,6 +116,29 @@
 ;; For emacsclient
 ;(server-start)
 
+;; *** Third party packages ***
+
+;; Git / Magit
+(require 'magit)
+(set-default 'magit-stage-all-confirm nil)
+(add-hook 'magit-mode-hook 'magit-load-config-extensions)
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(global-unset-key (kbd "C-x g"))
+(global-set-key (kbd "C-x g h") 'magit-log)
+(global-set-key (kbd "C-x g f") 'magit-file-log)
+(global-set-key (kbd "C-x g b") 'magit-blame-mode)
+(global-set-key (kbd "C-x g m") 'magit-branch-manager)
+(global-set-key (kbd "C-x g c") 'magit-branch)
+(global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g r") 'magit-reflog)
+(global-set-key (kbd "C-x g t") 'magit-tag)
+
 ;; Flycheck
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
