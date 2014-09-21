dcdb2d10b9985e2a39f421cd80457254b86cd030
Eddie Kovsky <ewk@edkovsky.org>
2014-09-20 23:57:00 -0600

    Emacs ELPA packages

---
+++ .emacs.d/elpa/epl-20140823.609/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21521 11403 969770
+;;;;;;  806000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
