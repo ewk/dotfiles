8f923ea330043d433465132b8dbdf2cd699ed3ef
Eddie Kovsky <ewk@edkovsky.org>
2014-12-27 22:33:54 -0700

    Got a working Emacs
    Syntax highlighting is finally decent. Autocomplete even works!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,46 +1,92 @@
 (set-language-environment "UTF-8")
 
-;; Load path
-(add-to-list 'load-path "~/.emacs.d/themes/")
-(add-to-list 'load-path "~/.emacs.d/modules/")
-;(add-to-list 'load-path "~/.emacs.d/modules/auto-complete-1.3.1")
+(setq user-full-name "Eddie Kovsky")
+(setq user-mail-address "ewk@edkovsky.org")
 
-;; Replace package archive with melpa
-(require 'package)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; Use Common Lisp
+(require 'cl)
+
+;; Add marmalade and melpa repositories
+(load "package")
 (package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+;; Default pacakges
+(defvar ewk/packages '(ac-slime
+                          auto-complete
+                          autopair
+                          flycheck
+                          go-mode
+                          magit
+                          marmalade
+                          org
+                          solarized-theme)
+  "Default packages")
+
+;; Install packages if not present
+(defun ewk/packages-installed-p ()
+  (loop for pkg in ewk/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (ewk/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg ewk/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Turn off scrash screen and scratch mode message
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
 
 ;; Appearance
 (set-default-font "Inconsolata-14" "Menlo-12")
-(require 'zenburn-theme)
 (column-number-mode 1) ; Show column number in mode-line
 (global-linum-mode 1) ; Line numbers in all buffers
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+
 ;; Window size
 (setq initial-frame-alist '((top . 10) (left . 30)
                             (width . 90) (height . 50)))
-
-;; Tabs and spaces
-(setq-default tab-width 8) ;; Set tab width
-(setq-default c-basic-offset 8) ;; Applies to C and most languages
-(setq-default cperl-indent-level 8) ;; Perl, of course, is different
-(setq-default indent-tabs-mode t) ; nil will use spaces instead of tabs
-;(setq sentence-end-double-space nil) ; Sentences end with one space
-(global-set-key (kbd "C-c w") 'whitespace-mode) ; view all whitespace characters
-;; show unncessary whitespace that can mess up your diff
-(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
-
 ;; Menu bar
+(tool-bar-mode -1) ;; Hide toolbar buttons
 (define-key menu-bar-tools-menu [games] nil)
-(tool-bar-mode 0)
-(setq inhibit-startup-message t) ; No message at startup
 
-;; enable electric pair in all buffers
-(electric-pair-mode t)
-(defun my-disable-electric-indentation ()
-  "Stop ';', '}', etc. from re-indenting the current line." 
-  (c-toggle-electric-state -1)) 
-(add-hook 'c-mode-common-hook 'my-disable-electric-indentation)
+;; Themes
+(if window-system
+    (load-theme 'solarized-dark t)
+  (load-theme 'wombat t))
+
+;; Show filename in frame
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
+;;(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;; Empty lines
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+;; Backups and autosave
+(setq make-backup-files nil) ; No backup files ~
+(setq auto-save-timeout 60) ; Autosave every minute
+
+;; Aliases
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Turn off beeping and improve keyboard response
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
 
 ;; Highlighting
 (show-paren-mode 1) ; Highlight parenthesis pairs
@@ -49,16 +95,28 @@
 (global-hl-line-mode t) ; Highlight cursor line
 (blink-cursor-mode 0)
 (setq visible-bell t)
+(require 'autopair) ;; Close pairs automatically
 
 ; Interactive do things- find file auto complete
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
 (ido-mode 1)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
 (icomplete-mode t) ; Completion in mini-buffer
 (setq confirm-nonexistent-file-or-buffer nil)
-;(global-set-key (kbd "C-x C-b") 'ibuffer) ; replace buffer management
 ; better way to enable ibuffer
 (defalias 'list-buffers 'ibuffer)
+
+;; Turn on autocomplete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Turn on spellchecking
+(setq flyspell-issue-welcome-flag nil)
+(if (eq system-type 'darwin)
+    (setq-default ispell-program-name "/usr/local/bin/aspell")
+  (setq-default ispell-program-name "/usr/bin/aspell"))
+(setq-default ispell-list-command "list")
 
 ;; Buffer management
 (setq pop-up-frame t) ; Buffers in separate frames
@@ -66,93 +124,13 @@
 (setq enable-recursive-minibuffers t) ; Stack minibuffers
 (setq large-file-warning-threshold nil) ; Don't warn opening large files
 
-;; Aliases
-(defalias 'yes-or-no-p 'y-or-n-p) ; y/n instead of yes/no
-;; (setq confirm-kill-emacs 'yes-or-no-p) ;; Never quit accidentally
+;; Tabs and spaces
+;(setq-default tab-width 8) ;; Set tab width
+;(setq-default c-basic-offset 8) ;; Applies to C and most languages
+;(setq-default cperl-indent-level 8) ;; Perl, of course, is different
+;(setq-default indent-tabs-mode t) ; nil will use spaces instead of tabs
+;(setq sentence-end-double-space nil) ; Sentences end with one space
+(global-set-key (kbd "C-c w") 'whitespace-mode) ; view all whitespace characters
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
-;; Backups and autosave
-(setq make-backup-files nil) ; No backup files ~
-(setq auto-save-timeout 60) ; Autosave every minute
-
-;; Print argument list of function you are writing
-(eldoc-mode 1)
-
-;; Default mode
-(setq default-major-mode 'text-mode) ; Text-mode is default mode
-(add-hook 'text-mode-hook 'turn-on-auto-fill) ; auto-formatting in text-mode
-
-;; unique buffer names
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-
-;; Got a wheel mouse!
-(mouse-wheel-mode 1)
-
-(setq kill-whole-line t)
-
-;; Some small hacks Chris Kirchen
-(setq user-mail-address "edkovsky@gmail.com")
-(setq display-time-24hr-format t)
-(display-time-mode 1)
-(setq isearch-lazy-highlight nil)
-(setq Info-use-header-line nil)
-(setq Info-fontify nil)
-(setq ps-paper-type 'a4)
-(setq kill-read-only-ok t)
-(setq diff-switches "-urN")
-
-;; (require ruby-mode)
-;; .rb and .rbx are Ruby files
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rbx$" . ruby-mode))
-
-;; go-mode
-;;(require 'go-mode)
-;;(add-hook 'go-mode-hook 'subword-mode)
-;;(add-hook 'go-mode-hook #'(lambda () (setq fill-column 79)))
-;;(add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
-
-;; Use a better perl mode
-(fset 'perl-mode 'cperl-mode)
-
-;; For emacsclient
-;(server-start)
-
-;; *** Third party packages ***
-
-;; Git / Magit
-(require 'magit)
-(set-default 'magit-stage-all-confirm nil)
-(add-hook 'magit-mode-hook 'magit-load-config-extensions)
-
-;; full screen magit-status
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(global-unset-key (kbd "C-x g"))
-(global-set-key (kbd "C-x g h") 'magit-log)
-(global-set-key (kbd "C-x g f") 'magit-file-log)
-(global-set-key (kbd "C-x g b") 'magit-blame-mode)
-(global-set-key (kbd "C-x g m") 'magit-branch-manager)
-(global-set-key (kbd "C-x g c") 'magit-branch)
-(global-set-key (kbd "C-x g s") 'magit-status)
-(global-set-key (kbd "C-x g r") 'magit-reflog)
-(global-set-key (kbd "C-x g t") 'magit-tag)
-
-;; Flycheck
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(require 'flycheck-tip)
-(flycheck-tip-use-timer 'verbose)
-
-;; company-mode autocomplete
-;(require 'company)
-;(add-hook 'after-init-hook 'global-company-mode)
-
-;; auto-complete mode
-;(add-to-list 'load-path "~/.emacs.d")
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;(require 'auto-complete-config)
-;(ac-config-default)
