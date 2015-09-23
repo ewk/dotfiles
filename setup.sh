20c73f8a582e5264c5762e09fd38c4306b963a9d
Eddie Kovsky <ewk@edkovsky.org>
2015-09-23 15:25:17 -0600

    Change local variable to lowercase
    Uppercase variables are reserved for the shell and may create conflicts.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-CWD="$(pwd)"
+cwd="$(pwd)"
 
 # Link home directory to config files in /dotfiles
-ln -s "$CWD"/.ackrc "$HOME"
-ln -s "$CWD"/.emacs.d "$HOME"
-ln -s "$CWD"/.gitconfig "$HOME"
-ln -s "$CWD"/.gitignore "$HOME"
-ln -s "$CWD"/.gvimrc "$HOME"
-ln -s "$CWD"/.rsyncignore "$HOME"
-ln -s "$CWD"/.vim "$HOME"
-ln -s "$CWD"/.vimrc "$HOME"
-ln -s "$CWD"/.zprofile "$HOME"
-ln -s "$CWD"/.zshenv "$HOME"
-ln -s "$CWD"/.zshrc "$HOME"
-ln -s "$CWD"/.mutt "$HOME"
-ln -s "$CWD"/.notmuch-config "$HOME"
+ln -s "$cwd"/.ackrc "$HOME"
+ln -s "$cwd"/.emacs.d "$HOME"
+ln -s "$cwd"/.gitconfig "$HOME"
+ln -s "$cwd"/.gitignore "$HOME"
+ln -s "$cwd"/.gvimrc "$HOME"
+ln -s "$cwd"/.rsyncignore "$HOME"
+ln -s "$cwd"/.vim "$HOME"
+ln -s "$cwd"/.vimrc "$HOME"
+ln -s "$cwd"/.zprofile "$HOME"
+ln -s "$cwd"/.zshenv "$HOME"
+ln -s "$cwd"/.zshrc "$HOME"
+ln -s "$cwd"/.mutt "$HOME"
+ln -s "$cwd"/.notmuch-config "$HOME"
 
 # Copy config and then enter password
 cp .muttrc "$HOME"
