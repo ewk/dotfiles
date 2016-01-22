ce949804303aea4c4b019a1b309539a03b409c54
Eddie Kovsky <ewk@edkovsky.org>
2016-01-21 22:57:26 -0700

    Move focus back to Emacs when emacsclient is invoked

--- .emacs.d/config/client-config.el
+++ .emacs.d/config/client-config.el
@@ -2,3 +2,12 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+;; Switch back to Emacs when another application invokes emacsclient
+(defun px-raise-frame-and-give-focus ()
+  (when window-system
+    (raise-frame)
+    (x-focus-frame (selected-frame))
+    (set-mouse-pixel-position (selected-frame) 4 4)
+    ))
+(add-hook 'server-switch-hook 'px-raise-frame-and-give-focus)
