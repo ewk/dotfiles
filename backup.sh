4a7f8c68ce5d6a276434504b391dab302fe6a6f3
Eddie Kovsky <ewk@edkovsky.org>
2016-02-04 23:46:21 -0700

    Fix incremental backup
    rsync was not configured correctly to descend into folders. Backups
    were missing changes in containing folders.

--- backup.sh
+++ backup.sh
@@ -25,6 +25,6 @@ date="$(date "+%Y-%m-%d-%H:%M:%S")"
 # remove the -$date suffix to use incremental backups
 rsync -aPv --exclude-from="$exclude" "$source" "$dest"/backup-"$date" > /dev/null &
 # Skips /etc/postfix/sasl_passwd
-rsync -Pv --exclude-from="$exclude" "$postfix" "$dest" > /dev/null &
-rsync -Pv --exclude-from="$exclude" "$logwatch" "$dest" > /dev/null &
+rsync -Pvr --exclude-from="$exclude" "$postfix" "$dest" > /dev/null &
+rsync -Pvr --exclude-from="$exclude" "$logwatch" "$dest" > /dev/null &
 
