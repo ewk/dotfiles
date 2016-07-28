ee32ca31942f6f29aa868ab899b672574f5c4884
Eddie Kovsky <ewk@edkovsky.org>
2016-07-27 19:22:53 -0600

    Add ctags to Emacs aliases

--- .emacs.d/config/alias-config.el
+++ .emacs.d/config/alias-config.el
@@ -7,3 +7,14 @@
 
 ;; better way to enable ibuffer
 (defalias 'list-buffers 'ibuffer)
+
+;; ctags
+(setq path-to-ctags "/usr/bin/etags")
+
+;; invoke with ‘m-x create-tags’
+(defun create-tags (dir-name)
+    "Create tags file."
+    (interactive "DDirectory: ")
+    (shell-command
+     (format "etags -f %s -e -R %s" path-to-ctags (directory-file-name dir-name)))
+    )
