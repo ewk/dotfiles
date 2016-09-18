b0f3f59dc449b0d8242782463a44eb90ebe27b55
Eddie Kovsky <ewk@edkovsky.org>
2016-09-17 22:57:34 -0600

    Emacs: Set up autocompletion

--- .emacs.d/config/autocomplete-config.el
+++ .emacs.d/config/autocomplete-config.el
@@ -1,16 +1,18 @@
-;;; Package --- Summary
+;;; autocomplete-config.el --- Summary
 
-;; Turn on autocomplete
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
-;(require 'auto-complete-config)
-;(ac-config-default)
-;(global-auto-complete-mode t)
-;(require 'go-autocomplete)
+;;; Commentary:
+
+;;; Code:
 
 ;; Turn on company mode
 (add-hook 'after-init-hook 'global-company-mode)
+
 ;; Trigger completion with TAB
-;(global-set-key "\t" 'company-complete-common)
+;(global-set-key "\t" 'company-complete)
+(setq company-tooltip-limit 15)
+(setq company-idle-delay 0)
+(setq company-echo-delay 0)
+(setq company-begin-commands '(self-insert-command))
 
 ; Interactive do things- find file auto complete
 (ido-mode 1)
@@ -19,3 +21,6 @@
       ido-use-virtual-buffers t)
 (icomplete-mode t) ; Completion in mini-buffer
 (setq confirm-nonexistent-file-or-buffer nil)
+
+(provide 'autocomplete-config)
+;;; autocomplete-config ends here
