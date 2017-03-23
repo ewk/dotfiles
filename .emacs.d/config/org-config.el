005c56c9bb2352fc062c102e3079bbcac8c04364
Eddie Kovsky <ewk@edkovsky.org>
2017-03-23 12:25:10 -0600

    Emacs: Finish org mode setup

--- .emacs.d/config/org-config.el
+++ .emacs.d/config/org-config.el
@@ -1,7 +1,14 @@
-;; org mode
+;;; org-config.el --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(define-key global-map "\C-cc" 'org-capture)
+(define-key global-map "\C-cb" 'org-iswitchb)
+
 ;; Fix org mode headers
 (custom-set-faces
   '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
@@ -9,4 +16,16 @@
   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-)
+  )
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
+
+;; Insert timestamp on items marked DONE
+(setq org-log-done 'time)
+
+;; Select :tag: with single keypress
+(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("laptop" . ?l)))
+
+(provide 'org-config)
+;;; org-config ends here
