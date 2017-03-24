6c2034b4fddf09d440dc945afef64c214dfa39c0
Eddie Kovsky <ewk@edkovsky.org>
2017-03-23 21:20:37 -0600

    Emacs: clean up init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,12 +5,10 @@
 
 ;;; Code:
 
-;; Use Common Lisp
 ;; Turn off scrollbars early in startup to avoid window width weirdness.
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (define-key menu-bar-tools-menu [games] nil)
-
 
 ;; Initialize must come before configurations installed packages.
 (package-initialize)
@@ -22,9 +20,7 @@
 (load "~/.emacs.d/config/appearance-config.el")
 (load "~/.emacs.d/config/buffer-config.el")
 (load "~/.emacs.d/config/autocomplete-config.el")
-(load "~/.emacs.d/config/whitespace-config.el")
 (load "~/.emacs.d/config/org-config.el")
-(load "~/.emacs.d/config/filetype-config.el")
 (load "~/.emacs.d/config/vc-config.el")
 (load "~/.emacs.d/config/alias-config.el")
 (load "~/.emacs.d/config/emacsclient-config.el")
