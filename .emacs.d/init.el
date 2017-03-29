3ef653a00f4be5636c6ee11e373350046cc3a65d
Eddie Kovsky <ewk@edkovsky.org>
2017-03-29 00:32:57 -0600

    Emacs: Move completion config to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -183,6 +183,28 @@
 ;; This may interfere with version control and next-line-add-newlines
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;;
+;; Completion
+;;
+
+;; Turn on company mode
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Trigger completion with TAB
+;(global-set-key "\t" 'company-complete)
+(setq company-tooltip-limit 15)
+(setq company-idle-delay 0)
+(setq company-echo-delay 0)
+(setq company-begin-commands '(self-insert-command))
+
+; Interactive do things- find file auto complete
+(ido-mode 1)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+(icomplete-mode t) ; Completion in mini-buffer
+(setq confirm-nonexistent-file-or-buffer nil)
+
 
 (load "~/.emacs.d/config/buffer-config.el")
 (load "~/.emacs.d/config/autocomplete-config.el")
