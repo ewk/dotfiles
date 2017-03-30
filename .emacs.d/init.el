85b4bda2de340c3fe54ecf4c72ed8256bb4c04db
Eddie Kovsky <ewk@edkovsky.org>
2017-03-29 21:33:25 -0600

    Emacs: Move emacsclient config to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -221,12 +221,34 @@
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
+;;
+;; Ctags
+;;
+
+;;
 ;; Alias customization
 ;;
 (load "~/.emacs.d/config/alias-config.el")
 
 (load "~/.emacs.d/config/org-config.el")
-(load "~/.emacs.d/config/emacsclient-config.el")
+
+;;
+;; Emacsclient
+;;
+;; Set 'EDITOR=emacsclient [-t]' to connect to the running process
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;; Switch back to Emacs when another application invokes emacsclient
+(defun px-raise-frame-and-give-focus ()
+  (when window-system
+    (raise-frame)
+    (x-focus-frame (selected-frame))
+    (set-mouse-pixel-position (selected-frame) 4 4)
+    ))
+(add-hook 'server-switch-hook 'px-raise-frame-and-give-focus)
+
 
 (provide 'init)
 ;;; init ends here
