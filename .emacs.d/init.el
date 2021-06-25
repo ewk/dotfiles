7c761cdd9afe1c13a65ffb17a050c024789d4899
Eddie Kovsky <ewk@edkovsky.org>
2021-06-24 21:42:46 -0600

    Emacs: burn it down and rebuild

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,10 +5,24 @@
 ;;; Code:
 
 ;;
+;; Package management
+;;
+(require 'package)
+(setq package-enable-at-startup nil)
+ (setq package-archives '(("melpa" . "https://melpa.org/packages/")))
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package)
+    (eval-when-compile (require 'use-package)))
+
+(setq use-package-always-ensure t)
+
+;;
 ;; General Config
 ;;
 
-(require 'cl-lib)
+(set-language-environment "UTF-8")
 
 ;; Turn off scrollbars early in startup
 (scroll-bar-mode -1)
@@ -17,56 +31,70 @@
 
 ;; Window size
 (setq initial-frame-alist '((top . 00) (left . 00)
-                            (width . 90) (height . 50)))
+                            (width . 105) (height . 50)))
+
 ;; Turn off splash screen and scratch mode message
 (setq inhibit-splash-screen t
       initial-scratch-message nil)
 
-;; Initialize must come before configurations installed packages.
-(load "~/.emacs.d/config/package-config.el")
-
-(set-language-environment "UTF-8")
-
-(column-number-mode 1) ; Show column number in mode-line
+(column-number-mode 1)               ; Show column number in mode-line
 (global-display-line-numbers-mode t) ; Line numbers in all buffers
 
 ;; Turn off beeping and improve keyboard response
-(blink-cursor-mode 0) ; Disable cursor blinking
+(blink-cursor-mode 0)        ; Disable cursor blinking
 (setq echo-keystrokes 0.1
       use-dialog-box nil
-      visible-bell t) ;; I am not a robot. I mean, the humans are dead.
+      visible-bell t)        ; I am not a robot. I mean, the humans are dead.
 
 ;; Show filename in frame
 (setq frame-title-format
         '(buffer-file-name "%f" (dired-directory dired-directory "%b")))
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 
+; Interactive do things- find file auto complete
+(ido-mode 1)
+(icomplete-mode t) ; Completion in mini-buffer
+(setq confirm-nonexistent-file-or-buffer nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Avoid backslash madness
+(require 're-builder)
+(setq reb-re-syntax 'string)
+
 ;; Buffer management
-(setq enable-recursive-minibuffers t) ; Stack minibuffers
-(setq large-file-warning-threshold nil) ; Don't warn opening large files
-(savehist-mode 1) ; Save minibuffer history
+(setq enable-recursive-minibuffers t)      ; Stack minibuffers
+(setq large-file-warning-threshold nil)    ; Don't warn opening large files
+(savehist-mode 1)                          ; Save minibuffer history
+
+;; Write all buffers when frame focus is lost
+(add-hook 'after-focuse-change-function (lambda () (save-some-buffers t)))
+(setq make-backup-files nil)    ; No backup files ~
+(global-auto-revert-mode t)     ; Reload files changed outside Emacs
+
+;; Remember last position in each file- .emacs.d/places must exist
+(save-place-mode 1)
+
+;; Prevent automatic changes to init.el
+(setq custom-file (make-temp-file "emacs-custom"))
+
+;;
+;; Appearance
+;;
 
 ;; Themes
-(load-theme 'material t)
-
-;; Cursorline
-(global-hl-line-mode t) ; Highlight cursor line
-(set-face-background 'hl-line "black") ; Set line highlight color
-(set-face-foreground 'highlight nil) ; Keep syntax highlighitng on highlighted line
+(use-package material-theme
+    :config
+    (load-theme 'material t))
 
 ;; Font
-(set-frame-font "Droid Sans Mono-15")
+(set-frame-font "Droid Sans Mono-16")
 
-;; TODO keep cursor at same point when scrolling; use C-l for now
+;; Disable syntax highlighting
+(global-font-lock-mode 0)
 
-;; Highlighting
-(electric-pair-mode t) ;; Close pairs automatically
-(setq blink-matching-paren t) ; Blinking parenthesis, on by default
-(transient-mark-mode t) ;; Highlight mark selection
-
-;; Editing
-(delete-selection-mode t) ;; Replace selected text
-(setq select-enable-clipboard t) ;; Interact with system clipboard
+;; Cursorline
+(global-hl-line-mode t)                  ; Highlight cursor line
+(set-face-background 'hl-line "black")  ; Set line highlight color
 
 ;; Fix terminal output characters
 (require 'ansi-color)
@@ -76,43 +104,56 @@
 (add-hook 'compilation-filter-hook 'my/ansi-colorize-buffer)
 
 ;;
-;; Search settings
+;; Programming
 ;;
 
-;; Avoid backslash madness
-(require 're-builder)
-(setq reb-re-syntax 'string)
+(use-package flycheck
+  :config
+  (add-hook 'after-init-hook 'global-flycheck-mode))
+
+;; assembly
+(require 'asm-mode)
+(add-hook 'asm-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode nil)    ; use spaces to indent
+	    (electric-indent-mode -1)      ; indentation in asm-mode is annoying
+	    (setq tab-stop-list (number-sequence 2 60 2))))
+(define-key asm-mode-map (kbd "<ret>") 'newline-and-indent)
+(define-key asm-mode-map (kbd "<backtab>") 'company-complete) ; complete with Shift-TAB
+
+;; Version control
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode t))
+
+;; Highlighting
+(electric-pair-mode t)           ; Close pairs automatically
+(show-paren-mode 1)
+(transient-mark-mode t)          ; Highlight mark selection
 
 ;;
-;; Backups and autosave
+;; Editing
 ;;
-(setq make-backup-files nil) ; No backup files ~
-;; Write all buffers when frame focus is lost, just like gVim
-(add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
-(global-auto-revert-mode t) ; Reload files changed outside Emacs
+(delete-selection-mode t)           ; Replace selected text
+(setq select-enable-clipboard t)    ; Interact with system clipboard
 
-;; Remember last position in each file- .emacs.d/places must exist
-(save-place-mode 1)
+;; Where is aspell?
+(setq-default flyspell-issue-welcome-flag nil)
+(setq-default ispell-program-name "/usr/bin/aspell")
+(setq-default ispell-list-command "list")
 
-;;
-;; Indentation
-;;
+;; Enable spell checking
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode) ;; Only checks comments
+(add-hook 'diff-mode-hook 'flyspell-mode)
 
 ;; Tabs and spaces
-(setq-default indent-tabs-mode t) ; Whether tabs or spaces are used for indentation
+(setq-default indent-tabs-mode t)
 
 ;; tab-width, c-basic-offset, and cperl-indent-level should always be the same
-(setq-default tab-width 8) ;; Width of a tab
-
-;; Indentation in CC mode applies to C as well as most other languages
+(setq-default tab-width 8)    ; Width of a tab
 (setq-default c-basic-offset 8)
 (setq-default cperl-indent-level 8) ;; Perl, of course, is different
-
-;; These languages can be fussy about whitespace
-;; C, Perl, sh, zsh, and mmix inherit CC mode
-;; Make must always use tabs, never spaces.
-(setq-default html-indent-level 2) ;; 2 soft spaces
-(setq-default css-indent-offset 4) ;; 4 space tabs
 
 ;; Sentences end with one space
 (setq sentence-end-double-space nil)
@@ -125,108 +166,8 @@
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
-;;
-;; Syntax and filetype
-;;
-(global-flycheck-mode) ;; Enable global syntax check
-
-;; Rust mode
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-(require 'asm-mode)
-(add-hook 'asm-mode-hook
-	  (lambda ()
-	    (setq indent-tabs-mode nil) ; use spaces to indent
-	    (electric-indent-mode -1) ; indentation in asm-mode is annoying
-	    (setq tab-stop-list (number-sequence 2 60 2))))
-(define-key asm-mode-map (kbd "<ret>") 'newline-and-indent)
-(define-key asm-mode-map (kbd "<backtab>") 'company-complete) ; complete with Shift-TAB
-
-;; Use Guile as our Scheme
-(setq-default scheme-program-name "guile")
-
-;; Set major mode for additional file types
-(add-to-list 'auto-mode-alist '("\\.gitconfig$" . conf-mode))
-(add-to-list 'auto-mode-alist '("rc$" . conf-mode))
-
-;; Version control
-(global-git-gutter-mode t)
-(add-to-list 'vc-handled-backends 'Git)
-
-;;
-;; Whitespace
-;;
-(load "~/.emacs.d/config/whitespace-config.el")
-
-;; Empty lines
-(setq-default indicate-empty-lines t)
-
 ;; This may interfere with version control and next-line-add-newlines
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;;
-;; Completion
-;;
-
-;; Turn on company mode
-(add-hook 'after-init-hook 'global-company-mode)
-
-; Interactive do things- find file auto complete
-(ido-mode 1)
-(icomplete-mode t) ; Completion in mini-buffer
-(setq confirm-nonexistent-file-or-buffer nil)
-
-;;
-;; Mode line
-;;
-
-;;
-;; Spell checking
-;;
-
-;; Where is aspell?
-(setq-default flyspell-issue-welcome-flag nil)
-(setq-default ispell-program-name "/usr/bin/aspell")
-(setq-default ispell-list-command "list")
-
-;; Enable spell checking
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode) ;; Only checks comments
-(add-hook 'diff-mode-hook 'flyspell-mode)
-
-;;
-;; Ctags
-;;
-
-;;
-;; Alias customization
-;;
-(load "~/.emacs.d/config/alias-config.el")
-
-;;
-;; Org mode
-;;
-(load "~/.emacs.d/config/org-config.el")
-
-;;
-;; Emacsclient
-;;
-;; Set 'EDITOR=emacsclient [-t]' to connect to the running process
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
-;; Switch back to Emacs when another application invokes emacsclient
-(defun px-raise-frame-and-give-focus ()
-  (when window-system
-    (raise-frame)
-    (x-focus-frame (selected-frame))
-    (set-mouse-pixel-position (selected-frame) 4 4)
-    ))
-(add-hook 'server-switch-hook 'px-raise-frame-and-give-focus)
-
-;; Prevent package-selected-packages from writing to init.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (provide 'init)
 ;;; init ends here
