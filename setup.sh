463008d500c4f4f99b4fbe4b9306a7dadc17d98b
Eddie Kovsky <ewk@edkovsky.org>
2023-04-12 21:11:05 -0600

    Remove path prefix from setup

--- setup.sh
+++ setup.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-cwd="$(pwd)"
-
 # Link home directory to config files in /dotfiles
-ln -sf "$cwd"/.gitconfig "$HOME"
-ln -sf "$cwd"/.gitignore "$HOME"
-ln -sf "$cwd"/.mutt "$HOME"
-ln -sf "$cwd"/.notmuch-config "$HOME"
-ln -sf "$cwd"/.rsyncignore "$HOME"
-ln -sf "$cwd"/.tmux.conf "$HOME"
-ln -sf "$cwd"/alacritty "$HOME"/.config
-ln -sf "$cwd"/backup.sh "$HOME/bin/"
-ln -sf "$cwd"/fish "$HOME"/.config
-ln -sf "$cwd"/nvim "$HOME"/.config
+ln -sf .gitconfig "$HOME"
+ln -sf .gitignore "$HOME"
+ln -sf .mutt "$HOME"
+ln -sf .notmuch-config "$HOME"
+ln -sf .rsyncignore "$HOME"
+ln -sf .tmux.conf "$HOME"
+ln -sf alacritty "$HOME"/.config
+ln -sf backup.sh "$HOME/bin/"
+ln -sf fish "$HOME"/.config
+ln -sf nvim "$HOME"/.config
 
 # Set up systemd timer for mbsync
 mkdir -p "$HOME"/.config/systemd/user
-ln -sf "$cwd"/mbsync.service  "$HOME"/.config/systemd/user
-ln -sf "$cwd"/mbsync.timer "$HOME"/.config/systemd/user
+ln -sf mbsync.service "$HOME"/.config/systemd/user
+ln -sf mbsync.timer "$HOME"/.config/systemd/user
 sudo systemctl daemon-reload
 systemctl --user start mbsync.timer
 systemctl --user enable mbsync.timer
