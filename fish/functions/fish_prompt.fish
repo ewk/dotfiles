3a0a601b3cbe4eb55c91286a5c35f0b65b156235
Eddie Kovsky <ewk@edkovsky.org>
2024-03-01 22:16:45 -0700

    Display non-zero exit code on prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt --description "minimal VCS prompt"
+    set -l _display_status $status
+
     set -g __fish_git_prompt_showcolorhints 1
     set -g __fish_git_prompt_showstashstate 1
     set -g __fish_git_prompt_showdirtystate 1
@@ -16,5 +18,12 @@ function fish_prompt --description "minimal VCS prompt"
     set_color normal
 
     printf '%s ' (__fish_git_prompt)
+
+    if test $_display_status -ne 0
+        set_color -o red
+        printf '%s' $_display_status
+        set_color normal
+    end
+
     printf "%s " $prompt_symbol
 end
