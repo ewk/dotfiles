cd31819c3674f0694b2426e268a4f68fde8dd56b
Eddie Kovsky <ewk@edkovsky.org>
2026-04-10 23:15:21 -0600

    Switch to Neovim native package manager

--- nvim/lua/plugins-config.lua
+++ nvim/lua/plugins-config.lua
@@ -1,21 +1,6 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    "windwp/nvim-autopairs",
-    "lewis6991/gitsigns.nvim",
-    "romus204/tree-sitter-manager.nvim",
-    { "miikanissi/modus-themes.nvim", priority = 1000 },
+vim.pack.add({
+    "https://github.com/windwp/nvim-autopairs",
+    "https://github.com/lewis6991/gitsigns.nvim",
+    "https://github.com/romus204/tree-sitter-manager.nvim",
+    "https://github.com/miikanissi/modus-themes.nvim",
 })
